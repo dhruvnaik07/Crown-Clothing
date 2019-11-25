58a78c8362ad9691d6ded88cac3061d7fbdac7ea
Dhruv Naik <dhruv3456@gmail.com>
2019-11-24 19:35:44 -0500

    made homepage into smaller components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.jsx'
+import HomePage from './Pages/HomePage/homepage.jsx'
 
 function App() {
 
---
+++ src/Pages/HomePage/homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.scss';
+import Directory from '../../Components/Directory/directory.jsx';
+
+const HomePage = () => (
+
+	<div className="homepage">
+		<Directory />
+	</div>
+);
+
+export default HomePage;
